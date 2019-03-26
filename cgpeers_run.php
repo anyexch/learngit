1d62ba2ee8e0ae1b303d8da10e5aacebc593cdfb
dsgsd <anyexch@gmail.com>
2019-03-26 17:03:23 +0800

    初始化

---
+++ cgpeers_run.php
@@ -0,0 +1,15 @@
+<?php 
+require 'cgpeers_test.php';
+/* function testAction(){
+        $url = 'https://www.cgpeers.com/torrents.php';
+        $post_data['action']       = 'download';
+        $post_data['id']      = '60272';
+        $post_data['authkey'] = '452b426bd9e35de08dc251d0297b9d6b';
+        $post_data['torrent_pass']    = 'd04jtipgbnive3g1dk525rtp5granc0s';
+        //$post_data = array();
+        $res = request_post($url,$ispost=true,$post_data);       
+        print_r($res);
+
+    }
+testAction(); */
+request_post(‘’,$ispost=true,array(""));
